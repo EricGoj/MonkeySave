2aca1244c1c2366866153b138cc58aefb49c2efe
Eric Quevedo <eric0quevedo2000@hotmail.com>
2025-08-20 23:54:02 -0300

    Refinamiento

---
+++ UI/.figma/1_5/index.jsx
@@ -0,0 +1,334 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.frame12}>
+      <div className={styles.sidebarOff}>
+        <div className={styles.logo}>
+          <img src="../image/mejdx5lp-3l4am52.svg" className={styles.logomark} />
+          <img src="../image/mejdx5lp-i6k0dj9.svg" className={styles.frame3} />
+        </div>
+        <div className={styles.collapse}>
+          <div className={styles.autoWrapper}>
+            <img
+              src="../image/mejdx5lp-0wjqfkq.png"
+              className={styles.collapseIcon}
+            />
+          </div>
+        </div>
+        <div className={styles.items}>
+          <div className={styles.sidebarItem01}>
+            <img
+              src="../image/mejdx5lp-bjusaha.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Dashboard</p>
+          </div>
+          <div className={styles.sidebarItem02}>
+            <img
+              src="../image/mejdx5lp-12ze2hq.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Actividad</p>
+          </div>
+          <div className={styles.sidebarItem2}>
+            <img
+              src="../image/mejdx5lp-sucpr0e.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Deudas</p>
+            <img src="../image/mejdx5lp-3b0r9r6.svg" className={styles.closed} />
+          </div>
+          <div className={styles.sidebarItem02}>
+            <img
+              src="../image/mejdx5lp-t5eno40.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Reservas</p>
+          </div>
+          <div className={styles.sidebarItem02}>
+            <img
+              src="../image/mejdx5lp-5acuc0u.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Noticias</p>
+          </div>
+          <div className={styles.sidebarItem02}>
+            <img
+              src="../image/mejdx5lp-786qer4.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Conversations</p>
+          </div>
+          <div className={styles.sidebarItem02}>
+            <img
+              src="../image/mejdx5lp-zkb9h2i.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.sidebarItem}>Export</p>
+          </div>
+        </div>
+        <div className={styles.user}>
+          <div className={styles.info}>
+            <img
+              src="../image/mejdx5lp-hta0tc1.png"
+              className={styles.rectangle1}
+            />
+            <div className={styles.frame1}>
+              <p className={styles.ericQuevedo}>Eric Quevedo</p>
+              <div className={styles.tag}>
+                <p className={styles.admin}>Admin</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame2}>
+            <div className={styles.sidebarItem3}>
+              <img
+                src="../image/mejdx5lp-86eslw9.svg"
+                className={styles.iconFromTablerIo}
+              />
+              <p className={styles.sidebarItem}>Ajustes</p>
+            </div>
+            <div className={styles.sidebarItem5}>
+              <img
+                src="../image/mejdx5lp-yd0fque.svg"
+                className={styles.iconFromTablerIo}
+              />
+              <p className={styles.sidebarItem4}>Log out</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.autoWrapper16}>
+        <div className={styles.contentArea}>
+          <div className={styles.bar}>
+            <img
+              src="../image/mejdx5lp-mphk8o5.svg"
+              className={styles.iconFromTablerIo}
+            />
+            <p className={styles.search}>Search</p>
+          </div>
+        </div>
+        <div className={styles.autoWrapper13}>
+          <div className={styles.autoWrapper5}>
+            <div className={styles.bg}>
+              <img src="../image/mejdx5lp-pqcxa1y.png" className={styles.icon} />
+              <div className={styles.autoWrapper2}>
+                <p className={styles.totalUser}>Total ahorro en pesos</p>
+                <p className={styles.a40689}>$ 40.689</p>
+                <div className={styles.a85UpFromYesterday}>
+                  <img
+                    src="../image/mejdx5lp-g8tlnxu.svg"
+                    className={styles.icTrendingUp24Px}
+                  />
+                  <p className={styles.a85UpFromYesterd3}>
+                    <span className={styles.a85UpFromYesterd}>8.5%</span>
+                    <span className={styles.a85UpFromYesterd2}>
+                      &nbsp;Rindio desde ayer
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.bg2}>
+              <div className={styles.autoWrapper4}>
+                <div className={styles.autoWrapper3}>
+                  <p className={styles.totalSales}>Pesos</p>
+                  <p className={styles.a89000}>$40.000</p>
+                </div>
+                <img src="../image/mejdx5lp-mdqiwen.svg" className={styles.icon2} />
+              </div>
+              <div className={styles.a85UpFromYesterday2}>
+                <img
+                  src="../image/mejdx5lp-2nhk0ce.svg"
+                  className={styles.icTrendingDown24Px}
+                />
+                <p className={styles.a43DownFromYeste3}>
+                  <span className={styles.a43DownFromYeste}>4.3%</span>
+                  <span className={styles.a85UpFromYesterd2}>&nbsp;</span>
+                  <span className={styles.a43DownFromYeste2}>
+                    Down from yesterday
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.autoWrapper10}>
+            <div className={styles.bg3}>
+              <div className={styles.autoWrapper7}>
+                <div className={styles.autoWrapper6}>
+                  <p className={styles.totalOrder}>Total ahorro en Dolares</p>
+                  <p className={styles.a10293}>US$ 38,293</p>
+                </div>
+                <img src="../image/mejdx5lp-kradcgf.png" className={styles.icon3} />
+              </div>
+              <div className={styles.a85UpFromYesterday3}>
+                <img
+                  src="../image/mejdx5lp-7g7elmq.svg"
+                  className={styles.icTrendingUp24Px2}
+                />
+                <p className={styles.a13UpFromPastWe}>
+                  <span className={styles.a85UpFromYesterd}>1.3%</span>
+                  <span className={styles.a85UpFromYesterd2}>&nbsp;</span>
+                  <span className={styles.a43DownFromYeste2}>
+                    Up from past week
+                  </span>
+                </p>
+              </div>
+            </div>
+            <div className={styles.bg4}>
+              <div className={styles.autoWrapper9}>
+                <div className={styles.autoWrapper8}>
+                  <p className={styles.totalUser2}>Dolares</p>
+                  <p className={styles.a406892}>US$ 8,000</p>
+                </div>
+                <img src="../image/mejdx5lp-7gi4nls.svg" className={styles.icon4} />
+              </div>
+              <div className={styles.a85UpFromYesterday4}>
+                <img
+                  src="../image/mejdx5lp-2aoykit.svg"
+                  className={styles.icTrendingUp24Px3}
+                />
+                <p className={styles.a85UpFromYesterd4}>
+                  <span className={styles.a85UpFromYesterd}>8.5%</span>
+                  <span className={styles.a85UpFromYesterd2}>
+                    &nbsp;Rindio desde ayer
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.contentCard}>
+            <div className={styles.carruselContent}>
+              <p className={styles.informacionDolar}>Informacion dolar</p>
+              <div className={styles.separator} />
+              <div className={styles.autoWrapper11}>
+                <p className={styles.actividad}>Actividad</p>
+                <div className={styles.separator2} />
+              </div>
+              <div className={styles.autoWrapper12}>
+                <p className={styles.reservas}>Reservas</p>
+                <div className={styles.separator3} />
+              </div>
+              <p className={styles.noticias}>Noticias</p>
+              <div className={styles.separator4} />
+              <div className={styles.menu}>
+                <p className={styles.a}>􀆊</p>
+              </div>
+            </div>
+            <div className={styles.slideIndicator}>
+              <div className={styles.dotIndictaor1}>
+                <div className={styles.dotIndictaor} />
+              </div>
+              <div className={styles.dotIndictaor1}>
+                <div className={styles.dotIndictaor} />
+              </div>
+              <div className={styles.dotIndictaor1}>
+                <div className={styles.dotIndictaor} />
+              </div>
+              <div className={styles.dotIndictaor2} />
+              <div className={styles.dotIndictaor1}>
+                <div className={styles.dotIndictaor} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.widget}>
+          <div className={styles.heading}>
+            <p className={styles.growth}>Growth</p>
+            <div className={styles.frame5}>
+              <p className={styles.yearly}>Yearly</p>
+              <img
+                src="../image/mejdx5lp-mticqf1.svg"
+                className={styles.iconFromTablerIo2}
+              />
+            </div>
+          </div>
+          <div className={styles.chart}>
+            <div className={styles.numbers}>
+              <p className={styles.a100K}>100k</p>
+              <p className={styles.a50K}>50k</p>
+              <p className={styles.a50K}>20k</p>
+              <p className={styles.a50K}>10k</p>
+              <p className={styles.a50K}>0</p>
+            </div>
+            <div className={styles.autoWrapper15}>
+              <img
+                src="../image/mejdx5lp-jd6f197.svg"
+                className={styles.maskGroup}
+              />
+              <div className={styles.years}>
+                <img src="../image/mejdx5lp-sgyw829.svg" className={styles.line3} />
+                <img
+                  src="../image/mejdx5lp-32n2t8y.svg"
+                  className={styles.line32}
+                />
+                <div className={styles.line33} />
+                <img
+                  src="../image/mejdx5lp-wx7l1y2.svg"
+                  className={styles.line34}
+                />
+                <img
+                  src="../image/mejdx5lp-hcprspr.svg"
+                  className={styles.line35}
+                />
+                <div className={styles.frame24}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2016}>2016</p>
+                </div>
+                <div className={styles.frame17}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2016}>2017</p>
+                </div>
+                <div className={styles.frame18}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2016}>2018</p>
+                </div>
+                <div className={styles.frame19}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2016}>2019</p>
+                </div>
+                <div className={styles.frame20}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2020}>2020</p>
+                </div>
+                <div className={styles.frame21}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2016}>2021</p>
+                </div>
+                <div className={styles.frame22}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2020}>2022</p>
+                </div>
+                <div className={styles.frame23}>
+                  <div className={styles.autoWrapper14}>
+                    <div className={styles.line36} />
+                  </div>
+                  <p className={styles.a2020}>2023</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
